cc7343e8100fd7017d2605fde0386e49b93bbd81
PaoloRollo <paolo.rollo1997@gmail.com>
2024-11-19 17:40:03 +0100

    feat: update.

--- src/agents/brian.ts
+++ src/agents/brian.ts
@@ -5,66 +5,14 @@ import {
 import { BrianToolkitOptions, BrianToolkit } from "@/toolkits";
 import { LanguageModelLike } from "@langchain/core/language_models/base";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { PostgresSaver } from "@langchain/langgraph-checkpoint-postgres";
 import { HandlerContext } from "@xmtp/message-kit";
 import { createToolCallingAgent, AgentExecutor } from "langchain/agents";
-import pg from "pg";
-import { createReactAgent as createLanggraphReactAgent } from "@langchain/langgraph/prebuilt";
-import { BaseChatModel } from "@langchain/core/language_models/chat_models";
-
-export type BrianMemoryOptions = {
-  shortTerm?: boolean;
-  longTerm?: {
-    postgres?: {
-      connectionString: string;
-    };
-  };
-  llm: BaseChatModel;
-};
 
 export type BrianAgentOptions = BrianToolkitOptions & {
   llm: LanguageModelLike;
   instructions?: string;
   xmtpHandler?: HandlerContext;
   xmtpHandlerOptions?: XMTPCallbackHandlerOptions;
-};
-
-export const createBrianAgentWithMemory = async ({
-  instructions,
-  apiKey,
-  apiUrl,
-  privateKeyOrAccount,
-  llm,
-  xmtpHandler,
-  xmtpHandlerOptions,
-  options,
-  shortTerm,
-  longTerm,
-}: BrianAgentOptions & BrianMemoryOptions) => {
-  let checkpointer;
-
-  const { tools } = new BrianToolkit({
-    apiKey,
-    apiUrl,
-    privateKeyOrAccount,
-    options,
-  });
-
-  if (longTerm?.postgres && longTerm.postgres.connectionString) {
-    const { Pool } = pg;
-    const pool = new Pool({
-      connectionString: "postgresql://user:password@localhost:5434/testdb",
-    });
-
-    checkpointer = new PostgresSaver(pool);
-    await checkpointer.setup();
-
-    const graph = createLanggraphReactAgent({
-      tools: tools,
-      llm: llm as BaseChatModel,
-      checkpointSaver: checkpointer,
-    });
-  }
 };
 
 export const createBrianAgent = async ({
